1906314f396c7e76766eb6288efd7fd97fc0f55a
doujianbin <23931216@qq.com>
2018-07-10 18:53:09 +0800

    Initial Commit

---
+++ GouKuRepast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GouKuRepast
+//
+//  Created by 窦建斌 on 2018/7/10.
+//  Copyright © 2018年 窦建斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GouKuRepast/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GouKuRepast
+//
+//  Created by 窦建斌 on 2018/7/10.
+//  Copyright © 2018年 窦建斌. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
